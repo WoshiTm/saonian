53b3040f7de3519c3ef3b3fa2644b8b968913035
WoshiTm <107546613+WoshiTm@users.noreply.github.com>
2024-10-29 11:04:41 +0800

    更新 music.js

--- music.js
+++ music.js
@@ -7,7 +7,7 @@ const headers = {
 
 const appConfig = {
   "ver": 1,
-  "name": "猫耳FM",
+  "name": "Music+FM",
   "message": "",
   "warning": "",
   "desc": "",
@@ -19,7 +19,7 @@ const appConfig = {
         "ui": 0,
         "showMore": true,
         "ext": {
-            "id": '1'
+            "gid": '1'
         }
     }, {
       "name": "白噪音",
@@ -27,7 +27,7 @@ const appConfig = {
       "ui": 0,
       "showMore": false,
       "ext": {
-          "id": '6'
+          "gid": '6'
       }
     }, {
         "name": "音单",
@@ -35,7 +35,7 @@ const appConfig = {
         "ui": 0,
         "showMore": true,
         "ext": {
-            "id": '2'
+            "gid": '2'
         }
     }, {
       "name": "播客",
@@ -43,34 +43,56 @@ const appConfig = {
       "ui": 1,
       "showMore": true,
       "ext": {
-          "id": '7'
+          "gid": '7'
       }
   }, {
-        "name": "歌单",
+        "name": "国风歌单",
         "type": "playlist",
         "ui": 1,
         "showMore": true,
         "ext": {
-            "id": '3'
+            "gid": '3'
         }
     }, {
+      "name": "流行歌单",
+      "type": "playlist",
+      "ui": 1,
+      "showMore": false,
+      "ext": {
+          "gid": '9'
+      }
+  }, {
       "name": "排行榜",
       "type": "playlist",
       "ui": 1,
       "showMore": true,
       "ext": {
-          "id": '4'
+          "gid": '4'
+      }
+    }, {
+      "name": "创作者",
+      "type": "artist",
+      "ui": 0,
+      "showMore": false,
+      "ext": {
+          "gid": '8'
       }
     }]
   },
   "tabMe": {
     "name": "我的",
     "groups": [{
-      "name": "歌曲",
+      "name": "红心",
       "type": "song"
     }, {
       "name": "歌单",
       "type": "playlist"
+    }, {
+      "name": "专辑",
+      "type": "album"
+    }, {
+      "name": "创作者",
+      "type": "artist"
     }]
   },
   "tabSearch": {
@@ -90,16 +112,15 @@ async function getConfig() {
 }
 
 async function getSongs(ext) {
-  const { page, id, from } = argsify(ext)
-  if (page > 1) {
-    return jsonify({
-      list: [],
-    })
-  }
-
+  const { page, gid, id, from, text } = argsify(ext)
   let songs = []
 
-  if (id == '1') {
+  if (gid == '1') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
     const { data } = await $fetch.get('https://www.missevan.com/sound/newhomepagedata', {
       headers
     })
@@ -123,7 +144,12 @@ async function getSongs(ext) {
     })
   }
 
-  if (id == '6') {
+  if (gid == '6') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
     songs = [{
       id: 'rs_1',
       name: 'Summer',
@@ -197,13 +223,45 @@ async function getSongs(ext) {
         vid: 'https://thesnds.b-cdn.net/sounds/ocean/calm-waves.mp3'
       }
     }]
+  }
 
-    return jsonify({
-      list: songs,
+  if (gid == '2') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
+    
+    const { data } = await $fetch.get(`https://www.missevan.com/sound/soundalllist?albumid=${id}`, {
+      headers
+    })
+    const info = argsify(data).info
+    let artist = {
+      id: `${info.owner.id}`,
+      name: info.owner.username,
+      cover: info.owner.avatar2
+    }
+    info.sounds.forEach( each => {
+      songs.push({
+        id: `${each.id}`,
+        name: each.soundstr,
+        cover: each.front_cover,
+        duration: parseInt(each.duration / 100),
+        artist: artist,
+        ext: {
+          url: each.soundurl
+        }
+      })
     })
   }
 
-  if (id == '5') {
+  if (gid == '5') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
+
     const { data } = await $fetch.get('http://www.htqyy.com/genre/1', {
       headers
     })
@@ -230,6 +288,160 @@ async function getSongs(ext) {
       })
     })
   }
+
+  if (gid == '9') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
+    const url = `https://c.y.qq.com/v8/fcg-bin/fcg_v8_playlist_cp.fcg?newsong=1&id=${id}&format=json&inCharset=GB2312&outCharset=utf-8`
+    const { data } = await $fetch.get(url, {
+      headers
+    })
+    const info = argsify(data).data.cdlist[0]
+    info.songlist.forEach((e) => {
+      songs.push({
+          id: e.mid,
+          name: e.name,
+          cover: e?.album?.mid ? `https://y.gtimg.cn/music/photo_new/T002R800x800M000${e.album.mid}.jpg` : '',
+          duration: e.interval ?? 0,
+          artist: {
+              id: e.singer[0].mid,
+              name: e.singer[0].name,
+              cover: `https://y.qq.com/music/photo_new/T001R500x500M000${e.singer[0].mid}.jpg`,
+          },
+          ext: {
+              qid: e.mid || e.songmid,
+          },
+      })
+    })
+  }
+
+  if (gid == '3') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
+    const { data } = await $fetch.get(`https://www.missevan.com/sound/soundalllist?albumid=${id}`, {
+      headers
+    })
+    const info = argsify(data).info
+    let artist = {
+      id: `${info.owner.id}`,
+      name: info.owner.username,
+      cover: info.owner.avatar2
+    }
+    info.sounds.forEach( each => {
+      songs.push({
+        id: `${each.id}`,
+        name: each.soundstr,
+        cover: each.front_cover,
+        duration: parseInt(each.duration / 100),
+        artist: artist,
+        ext: {
+          url: each.soundurl
+        }
+      })
+    })
+  }
+
+  if (gid == '4') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
+    let _a
+    const period = ext.period
+    const url = `https://u.y.qq.com/cgi-bin/musicu.fcg?g_tk=5381&data=%7B%22detail%22%3A%7B%22module%22%3A%22musicToplist.ToplistInfoServer%22%2C%22method%22%3A%22GetDetail%22%2C%22param%22%3A%7B%22topId%22%3A${id}%2C%22offset%22%3A0%2C%22num%22%3A100%2C%22period%22%3A%22${
+        (_a = period) !== null && _a !== void 0 ? _a : ''
+    }%22%7D%7D%2C%22comm%22%3A%7B%22ct%22%3A24%2C%22cv%22%3A0%7D%7D`
+    const { data } = await $fetch.get(url, {
+        headers: {
+            Cookie: 'uin=',
+        },
+    })
+    argsify(data).detail.data.songInfoList.forEach((e) => {
+      songs.push({
+          id: e.mid,
+          name: e.name,
+          cover: e?.album?.mid ? `https://y.gtimg.cn/music/photo_new/T002R800x800M000${e.album.mid}.jpg` : '',
+          duration: e.interval ?? 1,
+          artist: {
+              id: e.singer[0].mid,
+              name: e.singer[0].name,
+              cover: '',
+          },
+          ext: {
+              qid: e.mid || e.songmid,
+          },
+      })
+    })
+  }
+
+  if (gid == '8') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
+
+    const { data } = await $fetch.get(`http://c.y.qq.com/soso/fcgi-bin/client_search_cp?new_json=1&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=${page}&n=20&w=${encodeURIComponent(text)}&needNewCode=0`, {
+      headers
+    })
+
+    argsify(data.slice(9, -1)).data?.song?.list?.forEach( each => {
+      if (text === each.singer[0]?.name) {
+        songs.push({
+          id: `${each.mid}`,
+          name: each.name,
+          cover: `https://y.gtimg.cn/music/photo_new/T002R800x800M000${each.album.mid}.jpg`,
+          duration: 0,
+          artist: {
+            id: `${each.singer[0]?.id}`,
+            name: each.singer[0]?.name || '',
+          },
+          ext: {
+            qid: each.mid
+          }
+        })
+      }
+    })
+  }
+
+  if (gid == '7') {
+    if (page > 1) {
+      return jsonify({
+        list: [],
+      })
+    }
+    const { data } = await $fetch.get(id, {
+      headers
+    })
+    const $ = cheerio.load(data, { xmlMode: true })
+    const author = $('title:first').text()
+    const cover = $('itunes\\:image:first').attr('href') ?? ''
+    const artist = {
+      id: author,
+      name: author,
+      cover: cover,
+    }
+    $('item').each((index, each) => {
+      const ele = $(each)
+      songs.push({
+        id: ele.find('guid').text(),
+        name: ele.find('title').text(),
+        cover: ele.find('itunes\\:image').attr('href') ?? cover,
+        duration: 0,
+        artist,
+        ext: {
+            pid: ele.find('enclosure').attr('url') ?? ''
+        },
+      })
+    })
+  }
   
   return jsonify({
     list: songs,
@@ -237,23 +449,79 @@ async function getSongs(ext) {
 }
 
 async function getArtists(ext) {
-  const { page, id, from } = argsify(ext)
+  const { page, gid, from } = argsify(ext)
+  let artists = []
+  
+  if (page > 1) {
+    return jsonify({list: artists})
+  }
+
+  if (gid === '8') {
+    const { data } = await $fetch.get(`https://y.qq.com/n/ryqq/singer_list`, {
+      headers
+    })
+    const $ = cheerio.load(data)
+    $('li.singer_list__item').each((index, each) => {
+      const name = $(each).find('a').attr('title')
+      const id = $(each).find('a').attr('href').slice(15)
+      const cover = `https://y.qq.com/music/photo_new/T001R500x500M000${id}.jpg`
+      artists.push({
+        id,
+        name,
+        cover,
+        groups: [{
+          name: '热门歌曲',
+          type: 'song',
+          ext: {
+            gid: gid,
+            id: id,
+            text: name,
+          }
+        }]
+      })
+    })
+  }
+  
   return jsonify({
-    list: [],
+    list: artists,
   })
 }
 
 async function getPlaylists(ext) {
-  const { page, id, from } = argsify(ext)
+  const { page, gid, from } = argsify(ext)
   if (page > 1) {
     return jsonify({
       list: [],
     })
   }
+  
   let cards = []
 
+  if (gid == '9') {
+    const { data } = await $fetch.get('https://y.qq.com/n/ryqq/category', {
+      headers
+    })
+    let json = data.match(/__INITIAL_DATA__ =({.*?})<\/script>/)[1]
+    argsify(json).playlist.forEach( each => {
+      cards.push({
+        id: `${each.dissid}`,
+        name: each.dissname,
+        cover: each.imgurl,
+        artist: {
+          id: each.encrypt_uin,
+          name: each.creatorname
+        },
+        ext: {
+          gid,
+          id: `${each.dissid}`,
+          type: "playlist"
+        }
+      })
+    })
+  }
+
   // id 1 
-  if (id == '3') {
+  if (gid == '3') {
     const { data } = await $fetch.get('https://www.missevan.com/explore/tagalbum?order=0', {
       headers
     })
@@ -267,13 +535,14 @@ async function getPlaylists(ext) {
           name: each.username
         },
         ext: {
+          gid,
           id: each.id
         }
       })
     })
   }
 
-  if (id == '4') {
+  if (gid == '4') {
     const { data } = await $fetch.get(
       'https://u.y.qq.com/cgi-bin/musicu.fcg?_=1577086820633&data=%7B%22comm%22%3A%7B%22g_tk%22%3A5381%2C%22uin%22%3A123456%2C%22format%22%3A%22json%22%2C%22inCharset%22%3A%22utf-8%22%2C%22outCharset%22%3A%22utf-8%22%2C%22notice%22%3A0%2C%22platform%22%3A%22h5%22%2C%22needNewCode%22%3A1%2C%22ct%22%3A23%2C%22cv%22%3A0%7D%2C%22topList%22%3A%7B%22module%22%3A%22musicToplist.ToplistInfoServer%22%2C%22method%22%3A%22GetAll%22%2C%22param%22%3A%7B%7D%7D%7D',
       {
@@ -292,20 +561,21 @@ async function getPlaylists(ext) {
                 name: e.title,
                 cover: e.headPicUrl || e.frontPicUrl,
                 artist: {
-                    id: 'qq',
-                    name: '',
+                  id: 'qq',
+                  name: 'QQ',
                 },
                 ext: {
-                    id: `${e.topId}`,
-                    type: 'toplist',
-                    period: e.period,
+                  gid,
+                  id: `${e.topId}`,
+                  type: 'toplist',
+                  period: e.period,
                 },
             })
         })
     })
   }
 
-  if (id == '7') {
+  if (gid == '7') {
     const { data } = await $fetch.get('https://getpodcast.xyz/', {
       headers
     })
@@ -325,6 +595,7 @@ async function getPlaylists(ext) {
           name: name
         },
         ext: {
+          gid,
           type: 'podcast',
           id: url
         }
@@ -342,7 +613,7 @@ async function getPlaylists(ext) {
 }
 
 async function getAlbums(ext) {
-  const { page, id, from } = argsify(ext)
+  const { page, gid, from } = argsify(ext)
   if (page > 1) {
     return jsonify({
       list: [],
@@ -350,12 +621,10 @@ async function getAlbums(ext) {
   }
   let cards = []
 
-  // id 1 
-  if (id == '2') {
+  if (gid == '2') {
     const { data } = await $fetch.get('https://www.missevan.com/explore/tagalbum?order=0', {
       headers
     })
-    // $print(`***data: ${data}`)
     argsify(data).albums.forEach( each => {
       cards.push({
         id: `${each.id}`,
@@ -366,6 +635,7 @@ async function getAlbums(ext) {
           name: each.username
         },
         ext: {
+          gid,
           id: each.id
         }
       })
@@ -377,6 +647,7 @@ async function getAlbums(ext) {
   })
 }
 
+/*
 async function getPlaylistInfo(ext) {
   ext = argsify(ext)
   const { id, page, type } = ext
@@ -423,6 +694,44 @@ async function getPlaylistInfo(ext) {
     })
   }
 
+  if (type === 'playlist') {
+    const url = `https://c.y.qq.com/v8/fcg-bin/fcg_v8_playlist_cp.fcg?newsong=1&id=${id}&format=json&inCharset=GB2312&outCharset=utf-8`
+    const { data } = await $fetch.get(url, {
+      headers
+    })
+    const info = argsify(data).data.cdlist[0]
+    info.songlist.forEach((e) => {
+      songs.push({
+          id: e.mid,
+          name: e.name,
+          cover: e?.album?.mid ? `https://y.gtimg.cn/music/photo_new/T002R800x800M000${e.album.mid}.jpg` : '',
+          duration: e.interval ?? 0,
+          artist: {
+              id: e.singer[0].mid,
+              name: e.singer[0].name,
+              cover: `https://y.qq.com/music/photo_new/T001R500x500M000${e.singer[0].mid}.jpg`,
+          },
+          ext: {
+              qid: e.mid || e.songmid,
+          },
+      })
+    })
+
+    return jsonify({
+        item: {
+            id: `${info.dissid}`,
+            name: info.dissname,
+            cover: info.logo,
+            artist: {
+              id: `${info.uin}`,
+              name: info.nickname,
+              cover: info.headurl
+            },
+            songs,
+        },
+    })
+  }
+
   if (type === 'toplist') {
     let _a
     const period = ext.period
@@ -441,21 +750,21 @@ async function getPlaylistInfo(ext) {
     }
     let info = argsify(data).detail.data.data
     argsify(data).detail.data.songInfoList.forEach((e) => {
-        // $print(JSON.stringify(e, null, 2))
-        songs.push({
-            id: e.mid,
-            name: e.name,
-            cover: e?.album?.mid ? `https://y.gtimg.cn/music/photo_new/T002R800x800M000${e.album.mid}.jpg` : '',
-            duration: e.interval ?? 1,
-            artist: {
-                id: e.singer[0].mid,
-                name: e.singer[0].name,
-                cover: '',
-            },
-            ext: {
-                qid: e.mid || e.songmid,
-            },
-        })
+      // $print(JSON.stringify(e, null, 2))
+      songs.push({
+          id: e.mid,
+          name: e.name,
+          cover: e?.album?.mid ? `https://y.gtimg.cn/music/photo_new/T002R800x800M000${e.album.mid}.jpg` : '',
+          duration: e.interval ?? 1,
+          artist: {
+              id: e.singer[0].mid,
+              name: e.singer[0].name,
+              cover: '',
+          },
+          ext: {
+              qid: e.mid || e.songmid,
+          },
+      })
     })
 
     return jsonify({
@@ -542,13 +851,14 @@ async function getAlbumInfo(ext) {
     }
   })
 }
+*/
 
 async function search(ext) {
   const { text, page, type } = argsify(ext)
 
-  // if (page > 1) {
-  //   return jsonify({})
-  // }
+  if (page > 3) {
+    return jsonify({})
+  }
 
   if (type == 'song') {
     let songs = []
